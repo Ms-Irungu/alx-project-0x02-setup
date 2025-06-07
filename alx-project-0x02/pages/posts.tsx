c83d7c7a04eae82335b741faad778b589efb99bc
Valentine Irungu <irunguvalentine73@gmail.com>
2025-06-07 10:21:34 +0300

    Update Header component in the Pages

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from '@/components/layout/Header'
 
 const posts = () => {
   return (
-    <div>Posts will be viewed from here</div>
+    <div className='flex flex-col min-h-screen'>
+      <Header />
+      <main className='px-2 sm:px-4 md:px-6 mt-3'>
+        <h1 className='text-2xl font-bold'>Posts Page</h1>
+        <p className='mt-2'>This is where you can view all posts.</p>
+      </main>
+    </div>
   )
 }
 
